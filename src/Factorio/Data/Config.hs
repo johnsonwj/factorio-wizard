5d9980355b04ad3cddc8aaafb90fd9998dac97cd
William Johnson <johns.willj@gmail.com>
2017-08-02 09:50:29 -0700

    Use generics for config parsing

--- src/Factorio/Data/Config.hs
+++ src/Factorio/Data/Config.hs
@@ -1,17 +1,21 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveGeneric #-}
 
 module Factorio.Data.Config where
 
 import Control.Applicative
 import Data.Map.Strict (Map)
 import Data.Text
-import Data.Yaml (FromJSON(..), (.:), withObject)
+import Data.Yaml (FromJSON(..))
+import Data.Aeson.Types (Options, defaultOptions, camelTo2, fieldLabelModifier, genericParseJSON)
+import GHC.Generics
 
 type RecipeAlias = Text
 type RecipeName = Text
 type OutpostName = Text
 type ScienceType = Text
 
+type OnsiteRecipes = [RecipeName]
 type StockpilePhase = Map RecipeName Int
 type OutpostPlan = Map OutpostName [StockpilePhase]
 
@@ -20,39 +24,29 @@ type FuelName = Text
 type MineableName = Text
 type AssemblerName = Text
 
+jsonOptions :: Options
+jsonOptions = defaultOptions { fieldLabelModifier = camelTo2 '-' }
+
 data AdditionalData = AdditionalData { smeltingBurnerConsumption :: Map FurnaceName Float
                                      , fuelValues :: Map FuelName Float
                                      , smeltingSpeed :: Map FurnaceName Float
                                      , miningSpeed :: Map MineableName Float
                                      , craftingSpeed :: Map AssemblerName Float
-                                     } deriving (Show)
+                                     } deriving (Show, Generic)
 
 instance FromJSON AdditionalData where
-  parseJSON = withObject "AdditionalData" $ \v -> AdditionalData
-    <$> v .: "smelting-burner-consumption"
-    <*> v .: "fuel-values"
-    <*> v .: "smelting-speed"
-    <*> v .: "mining-speed"
-    <*> v .: "crafting-speed"
+  parseJSON = genericParseJSON jsonOptions
 
 data Config = Config { version :: Text
                      , labCount :: Int
                      , stockpilePeriod :: Int
                      , aliases :: Map RecipeAlias RecipeName
                      , science :: [ScienceType]
-                     , onsite :: [RecipeName]
+                     , onsite :: OnsiteRecipes
                      , outposts :: OutpostPlan
                      , additionalData :: AdditionalData
-                     } deriving (Show)
+                     } deriving (Show, Generic)
 
 instance FromJSON Config where
-  parseJSON = withObject "Config" $ \v -> Config
-    <$> v .: "version"
-    <*> v .: "lab-count"
-    <*> v .: "stockpile-period"
-    <*> v .: "aliases"
-    <*> v .: "science"
-    <*> v .: "onsite"
-    <*> v .: "outposts"
-    <*> v .: "additional-data"
+  parseJSON = genericParseJSON jsonOptions
                     
