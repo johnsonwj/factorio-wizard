b176012fce268b827e97f2200cd0dd373b0b4923
William Johnson <johns.willj@gmail.com>
2017-08-05 22:59:05 -0700

    Printing improvements. Raw calculations are off for some reason...

--- science_planner.py
+++ science_planner.py
@@ -1,6 +1,7 @@
 import yaml
 import math
-from pprint import pprint
+from itertools import count
+from operator import itemgetter
 
 with open('data/configuration.yaml') as yconfig:
     config = yaml.safe_load(yconfig)
@@ -10,37 +11,6 @@ with open('data/recipes.15.31.yaml') as yrecipes:
 
 with open('data/additional-data.yaml') as ydata:
     additional_data = yaml.safe_load(ydata)
-
-
-science_types = [
-    ('red science', 'science-pack-1'),
-    ('green science', 'science-pack-2'),
-    ('blue science', 'science-pack-3'),
-    ('black science', 'military-science-pack'),
-    ('purple science', 'production-science-pack'),
-    ('yellow science', 'high-tech-science-pack')
-]
-
-
-def ingredients(recipe_name):
-    if recipe_name in recipes:
-        return recipes[recipe_name]['ingredients']
-    return dict()
-
-
-def is_recipe_of_category(recipe_name, category):
-    if recipe_name not in recipes:
-        return False
-
-    recipe = recipes[recipe_name]
-    if 'category' in recipe:
-        return recipe['category'] == category
-
-    return False
-
-
-def is_smelting_recipe(recipe_name):
-    return is_recipe_of_category(recipe_name, 'smelting')
 
 
 class Schedule:
@@ -75,6 +45,112 @@ class Schedule:
 
     def items(self):
         return self._items.items()
+
+
+science_types = [
+    ('red science', 'science-pack-1'),
+    ('green science', 'science-pack-2'),
+    ('blue science', 'science-pack-3'),
+    ('black science', 'military-science-pack'),
+    ('purple science', 'production-science-pack'),
+    ('yellow science', 'high-tech-science-pack')
+]
+
+
+def ingredients(recipe_name):
+    if recipe_name in recipes:
+        return recipes[recipe_name]['ingredients']
+    return dict()
+
+
+def is_recipe_of_category(recipe_name, category):
+    if recipe_name not in recipes:
+        return False
+
+    recipe = recipes[recipe_name]
+    if 'category' in recipe:
+        return recipe['category'] == category
+
+    return False
+
+
+def is_smelting_recipe(recipe_name):
+    return is_recipe_of_category(recipe_name, 'smelting')
+
+
+def is_chemistry_recipe(recipe_name):
+    return is_recipe_of_category(recipe_name, 'chemistry')
+
+
+def crack_light(light_rate):
+    rec = recipes['light-oil-cracking']
+    frac = float(rec['result_count'])/rec['ingredients']['light-oil']
+    return light_rate * frac
+
+
+def crack_heavy_petrol(heavy_rate):
+    rec = recipes['heavy-oil-cracking']
+    frac = float(rec['result_count'])/rec['ingredients']['heavy-oil']
+    return crack_light(heavy_rate * frac)
+
+
+def crack_heavy_light(heavy_rate):
+    rec = recipes['heavy-oil-cracking']
+    frac = float(rec['result_count'])/rec['ingredients']['heavy-oil']
+    return heavy_rate * frac
+
+
+# todo: cracking
+def calculate_oil(dependency_tree):
+
+    chemistry_schedule = Schedule({ing['name']: ing['desired rate']
+                                   for ing in dependency_tree['ingredients']
+                                   if is_chemistry_recipe(ing['name'])})
+
+    chemistry_schedule.add('lubricant', 0.0)  # avoid key error next line
+    # lubricant_rate = chemistry_schedule['lubricant']
+
+    oil_products = Schedule()
+    for chem_recipe, rate in chemistry_schedule.items():
+        for ingredient, ing_count in ingredients(chem_recipe).items():
+            if ingredient in ['petroleum-gas', 'light-oil', 'heavy-oil']:
+                oil_products.add(ingredient, ing_count * rate)
+    if config['advanced-oil-processing']:
+        oil_recipe = recipes['basic-oil-processing']
+        # cracking_enabled = True
+    else:
+        oil_recipe = recipes['basic-oil-processing']
+        # cracking_enabled = False
+
+    fraction_rates = dict(oil_products.items())
+    crude_rates = dict()
+
+    def crude_frac(f):
+        return float(oil_recipe['results'][f])/oil_recipe['ingredients']['crude-oil']
+
+    for fraction in oil_recipe['results']:
+        if fraction not in fraction_rates:
+            crude_rates['fraction'] = 0.0
+        else:
+            crude_rates[fraction] = fraction_rates[fraction] / crude_frac(fraction)
+
+    limiting_frac, crude_rate = max(fraction_rates.items(), key=itemgetter(1))
+
+    if crude_rate > 0:
+        dependency_tree['schedule'].add('oil-yield', crude_rate * 10)
+
+    for ing_tree in dependency_tree['ingredients']:
+        calculate_oil(ing_tree)
+
+
+"""
+    if cracking_enabled:
+        crackable_heavy_rate = fraction_rates['heavy-oil'] - lubricant_rate
+        product_rates = {f: crude_rate * crude_frac(f) for f in fraction_rates}
+
+    if cracking_enabled and limiting_frac == 'light-oil':
+        crude_rate -= crack_heavy_light(crackable_heavy_rate)
+"""
 
 
 def crafting_effort(recipe_name):
@@ -144,6 +220,7 @@ def assembler2_required(recipe_name, recipe_rate):
 def build_dependency_tree(recipe_name, desired_rate):
     tree = dict()
     tree['name'] = recipe_name
+    tree['desired rate'] = desired_rate
     tree['ingredients'] = [build_dependency_tree(ingredient_name, ingredient_count * desired_rate)
                            for ingredient_name, ingredient_count in ingredients(recipe_name).items()]
 
@@ -163,28 +240,63 @@ def build_dependency_tree(recipe_name, desired_rate):
     for ing_tree in tree['ingredients']:
         total_schedule += ing_tree['schedule']
 
+    calculate_oil(tree)
+
     tree['total_schedule'] = total_schedule
 
     return tree
 
+production_cycle_length = config['production-cycle-length']
 
-def print_dependency_tree(tree, indent_level=0):
+
+def print_dependency_schedule(name, schedule, rate, indent_level=0):
     def is_interesting(v):
         if isinstance(v, Schedule):
             return len(v) > 0
         else:
             return v > 0
 
-    req_strings = ['{}: {}'.format(k, v) for k, v in tree['schedule'].items() if is_interesting(v)]
+    req_strings = ['{}: {}'.format(k, v) for k, v in schedule.items() if is_interesting(v)]
 
-    print('{}{} ({})'.format(' ' * indent_level, tree['name'], ', '.join(req_strings)))
+    print('{}{:>25s}: {:.2e}/cycle ({})'
+          .format(' ' * indent_level, name, rate * production_cycle_length, ', '.join(req_strings)))
+
+
+def print_dependency_tree(tree, indent_level=0):
+    print_dependency_schedule(tree['name'], tree['schedule'], tree['desired rate'], indent_level)
+    for ing_tree in tree['ingredients']:
+        print_dependency_tree(ing_tree, indent_level + 25)
+
+
+def recipe_totals(tree):
+    total_recipe_schedules = Schedule()
+    total_recipe_rates = Schedule()
+    total_recipe_schedules.add(tree['name'], tree['schedule'])
+    total_recipe_rates.add(tree['name'], tree['desired rate'])
 
     for ing_tree in tree['ingredients']:
-        print_dependency_tree(ing_tree, indent_level + 2)
+        ing_totals, ing_rates = recipe_totals(ing_tree)
+        total_recipe_schedules += ing_totals
+        total_recipe_rates += ing_rates
+
+    return total_recipe_schedules, total_recipe_rates
 
 science_pack_rate = config['lab-count'] / additional_data['lab-research-time']
 
-for display_name, sci_recipe_name in science_types:
-    print('=== {} ===\n'.format(display_name))
-    print_dependency_tree(build_dependency_tree(sci_recipe_name, science_pack_rate))
+for idx, (display_name, sci_recipe_name) in zip(count(0, 1), science_types):
+    print('=== {} ==='.format(display_name))
+
+    if idx > 2:  # advanced oil processing after blue science
+        config['advanced-oil-processing'] = True
+
+    dep_tree = build_dependency_tree(sci_recipe_name, science_pack_rate)
+    # print_dependency_tree(dep_tree)
+
+    print()
+
+    totals, rates = recipe_totals(dep_tree)
+    sorted_deps = sorted(dict(totals.items()), key=lambda rn: (rates[rn], -1 * totals[rn]['assemblers']))
+    for dep_rn in sorted_deps:
+        print_dependency_schedule(dep_rn, totals[dep_rn], rates[dep_rn])
+
     print()
